1b76216c58324816599afb907974e17db619dc92
Mohamed Reda soliman zahw <112642921+mohammedzahw@users.noreply.github.com>
2022-12-17 16:30:31 +0200

    Update BFS.py

--- BFS.py
+++ BFS.py
@@ -1,36 +1,33 @@
 import os
 
-base_path = os.path.dirname(os.path.abspath(__file__))
+base_path = os.path.dirname(os.path.abspath(_file_))
 
 with open(os.path.join(base_path, "Real_distance_between_cities.txt")) as data:
-    real_distances = eval(data.read())
+    real_distances=eval(data.read())
 
 with open(os.path.join(base_path, "Coordinates.txt")) as data:
-    coordinates = eval(data.read())
+    coordinates=eval(data.read())   
 
-
-# start city -> end city
+# start city to  end city
 def Bfs(start, goal):
-    queue = [[start]]
-    visited = []
+    queue=[[start]]
+    visited=[]
 
     while queue:
         path = queue.pop(0)
         node = path[-1]
-
-        if node in visited:
+        
+        if node  in visited:
             continue
 
         visited.append(node)
 
-        if node == goal:
+        if node==goal :
             return path
         else:
-            adjacent_nodes = real_distances.get(node, [])
+            adjacent_nodes = real_distances.get(node,[])
             for node2 in adjacent_nodes:
                 new_path = path.copy()
                 new_path.append(node2)
                 queue.append(new_path)
-
-
 
