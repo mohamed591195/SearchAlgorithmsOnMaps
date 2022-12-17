8a3c8b5f14d16cf6b1a719b1ed10aa4a6acf0e85
Mostafa Wael <mostafawael9363@gmail.com>
2022-12-17 17:00:00 +0200

    My changes

--- BFS.py
+++ BFS.py
@@ -11,27 +11,36 @@ with open(os.path.join(base_path, "Coordinates.txt")) as data:
 
 # start city -> end city
 def Bfs(start, goal):
+    # we want to travers city level by level, so we use stack based on (first in first out)
+    # paths
     queue = [[start]]
+
+    # make it to check when city is visited or not
     visited = []
 
     while queue:
+        # find first path we constructed and remove it from queue
         path = queue.pop(0)
+
+        # find last node in path
         node = path[-1]
 
+        # check last node is visited or not
         if node in visited:
             continue
 
+        # add node to visited list
         visited.append(node)
 
+        # check if node is goal or not
         if node == goal:
             return path
-        else:
-            adjacent_nodes = real_distances.get(node, [])
-            for node2 in adjacent_nodes:
-                new_path = path.copy()
-                new_path.append(node2)
-                queue.append(new_path)
 
-
-
-
+        # find neighbors cities from last node
+        adjacent_nodes = real_distances.get(node, [])
+        for node2 in adjacent_nodes:
+            # construct new path by copying path and add to it neighbor nodes
+            new_path = path.copy()
+            new_path.append(node2)
+            # add new path to the top of queue
+            queue.append(new_path)
--- DFS.py
+++ DFS.py
@@ -7,27 +7,38 @@ base_path = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(base_path, 'Real_distance_between_cities.txt')) as data:
     cities_real_distance = eval(data.read())
 
-# algo based on straight forward pass
+# algorithm based on straight forward path
 
-
+# start city -> end city
 def DFS(start, goal):
+    # we want to travers nodes as we can, so we use stack based on (last in first out)
+    # paths
     stack = [[start]]
     visited = []
 
     while stack:
+        # find last path we constructed and remove it from stack
         path = stack.pop(-1)
+
+        # find last node in path
         node = path[-1]
 
+        # check last node is visited or not
         if node in visited:
             continue
 
+        # add node to visited list
         visited.append(node)
 
+        # check if node is goal or not
         if node == goal:
             return path
-        else:
-            adjacent_nodes = cities_real_distance.get(node, [])
-            for node2 in adjacent_nodes:
-                new_path = path.copy()
-                new_path.append(node2)
-                stack.append(new_path)
+
+        # find neighbors cities from last node
+        adjacent_nodes = cities_real_distance.get(node, [])
+        for node2 in adjacent_nodes:
+            # construct new path by copying path and add to it neighbor nodes
+            new_path = path.copy()
+            new_path.append(node2)
+            # add new path to the top of stack
+            stack.append(new_path)
